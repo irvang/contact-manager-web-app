817bbf5b5eeaceede7d5e976df48ebcdac8b73d5
Irving Angulo <irvang.manage@gmail.com>
2018-05-13 12:59:27 -0400

    Adapted and implemented sortTable() from W3Cs website. Added listeners.'

--- public/js/add-listeners.js
+++ public/js/add-listeners.js
@@ -1,3 +1,13 @@
+//post button
 document.querySelector('#post-btn').addEventListener('click', postFetch);
 
+//get button
 document.querySelector('#get').addEventListener('click', getFetch);
+
+//table sorting
+document.querySelector('#th-firstName').addEventListener('click', sortTable(0));
+document.querySelector('#th-lastName').addEventListener('click', sortTable(1));
+document.querySelector('#th-phone').addEventListener('click', sortTable(2));
+document.querySelector('#th-email').addEventListener('click', sortTable(3));
+document.querySelector('#th-dob').addEventListener('click', sortTable(4));
+document.querySelector('#th-notes').addEventListener('click', sortTable(5));
