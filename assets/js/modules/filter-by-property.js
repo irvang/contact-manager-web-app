fe6a7522c4e3ab164b3b6e84c0a6f286d6df00b9
Irving Angulo <irvang.manage@gmail.com>
2019-05-30 13:13:53 -0400

    Using class displayNone instead of applying stile inline to hide table rows.

--- assets/js/modules/filter-by-property.js
+++ assets/js/modules/filter-by-property.js
@@ -2,27 +2,31 @@
 	Adapted from https://www.w3schools.com/howto/howto_js_filter_table.asp
 */
 
-
-
 export default function filterByProperty(evt) {
-	//number (0..3) that will decide which table element to sort
+	//number (0..3) that will decide which td to sort
+	//select#propToFilter > option
 	let propToFilter = parseInt(document.querySelector('#propToFilter').value);
 
 	// Declare variables 
-	var input, filter, table, tr, td, i;
-	input = document.getElementById("search-input");
-	filter = input.value.toUpperCase();
-	table = document.getElementById("contactsTable");
-	tr = table.getElementsByTagName("tr");
+	let input = document.getElementById("search-input");
+	let filter = input.value.toUpperCase();
+	let table = document.getElementById("contactsTable");//selecting table
+	// let table = document.querySelector("#contactsTable>tbody");//selecting tbody
+	let tr = table.getElementsByTagName("tr"); // HTML collection, all rows
 
-	// Loop through all table rows, and hide those who don't match the search query
-	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName("td")[propToFilter];
+
+	// Loop through all table rows, and hide those that don't match the search query
+	for (let i = 0; i < tr.length; i++) {
+
+		// select specific td from tr (not from document), and apply filter to it
+		// using td ensures we do not hide thead. We could also only select tbody instead
+		let td = tr[i].getElementsByTagName("td")[propToFilter];
+
 		if (td) {
 			if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-				tr[i].style.display = "";
+				tr[i].classList.remove('displayNone'); // tr[i].style.display = "";
 			} else {
-				tr[i].style.display = "none";
+				tr[i].classList.add('displayNone'); // tr[i].style.display = "none";
 			}
 		}
 	}
